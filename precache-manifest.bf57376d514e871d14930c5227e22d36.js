91ba9663fb4a713b783f2c072855b64a1be2699f
Najwab <inajjwa2@gmail.com>
2020-02-26 16:06:17 +0300

    Updates

---
+++ precache-manifest.bf57376d514e871d14930c5227e22d36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac393b41e837706513d2b8d0f2982ed",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "75e65b139127fcae35af",
+    "url": "/music-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "27d8d3999b61b2db2f29",
+    "url": "/music-app/static/css/main.d66b01e8.chunk.css"
+  },
+  {
+    "revision": "75e65b139127fcae35af",
+    "url": "/music-app/static/js/2.a7478ab2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/music-app/static/js/2.a7478ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d8d3999b61b2db2f29",
+    "url": "/music-app/static/js/main.ec11a971.chunk.js"
+  },
+  {
+    "revision": "b79320c877b8b0264751",
+    "url": "/music-app/static/js/runtime-main.1d07188f.js"
+  }
+]);
